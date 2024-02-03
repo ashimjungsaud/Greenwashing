1de7a9e738e1c87bca34c8ea4eea5d522b359e8d
Debaditya <ddev2636@gmail.com>
2024-02-03 11:27:23 +0530

    contract updated

--- web3/contracts/GreenWashing.sol
+++ web3/contracts/GreenWashing.sol
@@ -6,8 +6,6 @@ contract CrowdFunding {
         address owner;
         string title;
         string description;
-        uint256 target;
-        uint256 deadline;
         uint256 amountCollected;
         string image;
         address[] donators;
@@ -31,40 +29,25 @@ contract CrowdFunding {
         address _owner,
         string memory _title,
         string memory _description,
-        uint256 _target,
-        uint256 _deadline,
         string memory _image
     ) public returns (uint256) {
         Campaign storage campaign = campaigns[numberOfCampaigns];
 
-        require(
-            _deadline > block.timestamp,
-            "The deadline should be a date in the future."
-        );
-
         campaign.owner = _owner;
         campaign.title = _title;
         campaign.description = _description;
-        campaign.target = _target;
-        campaign.deadline = _deadline;
         campaign.amountCollected = 0;
         campaign.image = _image;
-
         numberOfCampaigns++;
-
         return numberOfCampaigns - 1;
     }
 
     function donateToCampaign(uint256 _id) public payable {
         uint256 amount = msg.value;
-
         Campaign storage campaign = campaigns[_id];
-
         campaign.donators.push(msg.sender);
         campaign.donations.push(amount);
-
         (bool sent, ) = payable(campaign.owner).call{value: amount}("");
-
         if (sent) {
             campaign.amountCollected = campaign.amountCollected + amount;
         }
@@ -73,19 +56,12 @@ contract CrowdFunding {
     function upvoteCampaign(uint256 _id) public {
         require(_id < numberOfCampaigns, "Campaign does not exist");
         Campaign storage campaign = campaigns[_id];
-        // Check if campaign has exceeded its deadline
-        if (block.timestamp > campaign.deadline) {
-            // Delete the campaign by copying the last campaign to this index and decreasing the count
-            numberOfCampaigns--;
-            if (_id < numberOfCampaigns) {
-                campaigns[_id] = campaigns[numberOfCampaigns];
-            }
-            return; // Stop execution as the campaign has been deleted
-        }
+
         require(
             !hasVoted[_id][msg.sender],
             "You have already voted for this campaign"
         );
+
         Vote memory newVote = Vote({voter: msg.sender, isUpvote: true});
         votes[_id].push(newVote);
         hasVoted[_id][msg.sender] = true;
@@ -95,18 +71,7 @@ contract CrowdFunding {
     function downvoteCampaign(uint256 _id) public {
         require(_id < numberOfCampaigns, "Campaign does not exist");
         Campaign storage campaign = campaigns[_id];
-        if (block.timestamp > campaign.deadline) {
-            // Delete the campaign by copying the last campaign to this index and decreasing the count
-            numberOfCampaigns--;
-            if (_id < numberOfCampaigns) {
-                campaigns[_id] = campaigns[numberOfCampaigns];
-            }
-            return; // Stop execution as the campaign has been deleted
-        }
-        require(
-            campaign.deadline > block.timestamp,
-            "Voting is not allowed after the campaign deadline"
-        );
+
         require(
             !hasVoted[_id][msg.sender],
             "You have already voted for this campaign"
@@ -132,23 +97,10 @@ contract CrowdFunding {
     }
 
     function getCampaigns() public view returns (Campaign[] memory) {
-        Campaign[] memory activeCampaigns = new Campaign[](numberOfCampaigns);
-        uint256 activeCampaignsCount = 0;
+        Campaign[] memory result = new Campaign[](numberOfCampaigns);
 
         for (uint i = 0; i < numberOfCampaigns; i++) {
-            Campaign storage item = campaigns[i];
-
-            // Only include campaigns that have not passed their deadlines
-            if (block.timestamp <= item.deadline) {
-                activeCampaigns[activeCampaignsCount] = item;
-                activeCampaignsCount++;
-            }
-        }
-
-        // Create a new array with only active campaigns
-        Campaign[] memory result = new Campaign[](activeCampaignsCount);
-        for (uint i = 0; i < activeCampaignsCount; i++) {
-            result[i] = activeCampaigns[i];
+            result[i] = campaigns[i];
         }
 
         return result;
--- web3/hardhat.config.js
+++ web3/hardhat.config.js
@@ -1,14 +1,14 @@
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: {
-    version: '0.8.9',
-    defaultNetwork: 'goerli',
+    version: "0.8.17",
+    defaultNetwork: "sepolia", // Change default network to "sepolia"
     networks: {
       hardhat: {},
-      goerli: {
-        url: 'https://rpc.ankr.com/eth_goerli',
-        accounts: [`0x${process.env.PRIVATE_KEY}`]
-      }
+      sepolia: {  // Change network name to "sepolia"
+        url: "https://rpc.sepolia.com", // Replace with the actual URL for Sepolia
+        accounts: [`0x${process.env.PRIVATE_KEY}`],
+      },
     },
     settings: {
       optimizer: {
