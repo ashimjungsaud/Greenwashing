cb65cf0f078f485b432d700097c7ac1c6874ddde
ashim <ashimjs767@gmail.com>
2024-02-05 01:02:56 +0530

    Basic changes

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -5,7 +5,7 @@ import ContentSection1 from '../components/ContentSection1';
 import ContentSection2 from '../components/ContentSection2';
 import { img1, img2, img3 } from '../assets/index.js';
 import ContentSection3 from '../components/ContentSection3.jsx';
-import { Link } from 'react-scroll';
+import { Link, Element } from 'react-scroll';
 
 
 function LandingPage() {
@@ -19,18 +19,21 @@ function LandingPage() {
                 <div className='flex justify-between items-end lg:mt-[110px]'>
                     <About />
                 </div>
-                
+
                 <div className='ml-10'>
-                <Link to='Information' className="bg-primary font-epilogue font-semibold text-[20px] leading-[26px] text-white min-h-[52px] px-4 py-4 rounded-[10px]" spy={true} smooth={true} offset={50} duration={500}>Learn more</Link>
+                    <Link to='Information' className="bg-primary font-epilogue font-semibold text-[20px] leading-[26px] text-white min-h-[52px] px-4 py-4 rounded-[10px]" spy={true} smooth={true} offset={50} duration={500}>Learn more</Link>
                 </div>
             </div>
 
-            <div>
-                <ContentSection1 id="Information" img={img1} heading="What is Greenwashing ?" para="Greenwashing isn't about cleaning your lawn—it's about misleading marketing. Companies use it to make you think their products are environmentally friendly when they aren't." />
-                <ContentSection2 img={img2} heading="Why even care about it?" />
-                <ContentSection3 img={img3} heading="What Can We Do About It?" para="Be savvy shoppers, demand transparency, support ethical brands & sign petitions - together we can expose deception & drive real change!" />
+            <Element name="Information" className="element">
+                <div>
 
-            </div>
+                    <ContentSection1 img={img1} heading="What is Greenwashing ?" para="Greenwashing isn't about cleaning your lawn—it's about misleading marketing. Companies use it to make you think their products are environmentally friendly when they aren't." />
+                    <ContentSection2 img={img2} heading="Why even care about it?" />
+                    <ContentSection3 img={img3} heading="What Can We Do About It?" para="Be savvy shoppers, demand transparency, support ethical brands & sign petitions - together we can expose deception & drive real change!" />
+                </div>
+
+            </Element>
         </div>
 
     );
